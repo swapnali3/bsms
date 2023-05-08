56ca68e2fac0cf9fd367ceb2b8558f4709356a4c
snehal-ft <snehalb@fts-pl.com>
2023-05-08 16:53:48 +0530

    Design Gate Entry pages

--- templates/Buyer/Asn/search.php
+++ templates/Buyer/Asn/search.php
@@ -13,21 +13,27 @@
     <div class="col-12">
         <div class="card">
             <?= $this->Form->create() ?>
-            <!-- <div class="card-header">
-            <h5><b>ENTER ASN NO</b></h5>
-            </div> -->
+            <div class="card-header">
+            <h4 class="p-2 text-info"><b>Gate Entry (GE)</b></h3>
+            </div>
             <div class="card-body gate-entry">
                 <div class="row">
                 <!-- <div class="col-sm-12 col-md-6">
                 <h5>Enter ASN No</h5>
                 </div> -->
-                    <div class="col-sm-12 col-md-6">
-                    <h5 class="mb-2 gate-entry-haed font-weight-bold">ENTER ASN NO :</h5>
-                        <?php echo $this->Form->control('asn_no', array('label' => false, 'class' => 'form-control rounded-0','div' => 'form-group', 'required')); ?>
+                    <div class="col-sm-12 col-md-12">
+                       <div class="content-box">
+                       <img  class="scanner-img" src="<?= $this->Url->build('/')  ?>img/barcode-scanner.png"> 
+                        <br>  
+                        <span>Waiting for ASN Barcode to be scanned...</span>  
+                        <p><b>OR</b></p>
+                        <!-- <h5 class="mb-2 gate-entry-haed font-weight-bold">ENTER ASN NO :</h5> -->
+                            <?php echo $this->Form->control('asn_no', array('label' => false, 'class' => 'form-control rounded-0', "placeholder"=>"Start entering ASN Number",'div' => 'form-group', 'required')); ?>
+                            <?= $this->Form->button(__('GO'), ['class' => 'btn btn-info btn-block mt-3']) ?>
+                       </div>
                     </div>
-                    <div class="col-sm-12 col-md-12 mt-4">
-                        <?= $this->Form->button(__('Submit'), ['class' => 'btn btn-info']) ?>
-                    </div>
+                       
+                   
                 </div>
                 <?= $this->Form->end() ?>
             </div>
