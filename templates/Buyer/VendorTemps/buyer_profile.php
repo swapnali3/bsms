12e06d9e18fd40070af0d2a1eece0b4dfabde56a
snehal-ft <snehalb@fts-pl.com>
2023-05-18 16:12:39 +0530

    Design new buyer profile page and vendor view page
    1.new file created buyer profile page
    2.In Vendor login-vendor view page added field contact department and designation and address.
    3.design approve ,reject button

---
+++ templates/Buyer/VendorTemps/buyer_profile.php
@@ -0,0 +1,63 @@
+<div class="buyer=profile">
+<div class="row">
+    <div class="col-12">
+        <div class="vendorTemps view content card">
+            <div class="card-header">
+                <h5>
+                    <b>
+                        <?= h($vendorTemp->name) ?>
+                    </b>
+                </h5>
+            </div>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <table class="table vendor-info mb-0">
+
+                                <tr>
+                                    <th>
+                                        <?= __('Name') ?>
+                                    </th>
+                                    <td>
+                                        Test Name
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <?= __('Mobile No') ?>
+                                    </th>
+                                    <td>
+                                       98765432
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <?= __('Email Id') ?>
+                                    </th>
+                                    <td>
+                                        test@gmail.com
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <?= __('Address') ?>
+                                    </th>
+                                    <td>
+                                        Mumbai
+                                    </td>
+                                </tr>
+
+
+
+
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+</div>
