efc9dce78d2dd5784d9874f214c11ab0e071e656
abhishekfts <abhisheky@fts-pl.com>
2023-06-02 19:01:28 +0530

    notification add in create sechdule done

---
+++ src/Model/Entity/Notification.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Notification Entity
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property int $message_count
+ * @property string $notification_type
+ * @property \Cake\I18n\FrozenTime $created_at
+ *
+ * @property \App\Model\Entity\User $user
+ */
+class Notification extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array<string, bool>
+     */
+    protected $_accessible = [
+        'user_id' => true,
+        'message_count' => true,
+        'notification_type' => true,
+        'created_at' => true,
+        'user' => true,
+    ];
+}
