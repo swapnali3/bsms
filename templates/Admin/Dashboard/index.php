db6e916ee67f8d25d2ee6cd8d2a243b4a12c7464
snehal-ft <snehalb@fts-pl.com>
2023-06-07 14:07:00 +0530

    testing

--- templates/Admin/Dashboard/index.php
+++ templates/Admin/Dashboard/index.php
@@ -5,31 +5,29 @@
  */
 ?>
 
-  <!-- Main content -->
-  <section class="content">
 
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-12">
-          <!-- Default box -->
-          <div class="card">
-            <div class="card-header">
-              <h3 class="card-title">Subscriptions</h3>
-              <div class="card-tools">
-                <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                  <i class="fas fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                  <i class="fas fa-times"></i>
-                </button>
-              </div>
-            </div>
-            <div class="card-body">
-              Start Date: 01/01/2023 
+<!-- <section class="content">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12">
+        
+        <div class="card">
+          <div class="card-header">
+            <h3 class="card-title">Subscriptions</h3>
+            <div class="card-tools">
+              <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+                <i class="fas fa-minus"></i>
+              </button>
+              <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+                <i class="fas fa-times"></i>
+              </button>
             </div>
           </div>
-          <!-- /.card -->
+          <div class="card-body">
+            Start Date: 01/01/2023
+          </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
+</section> -->
