ad929b04cce6f5bb342bdc4bcf2b6469e5c99c3b
jthayil-ft <jonest@fts-pl.com>
2023-06-08 15:16:00 +0530

    Sidemenu

--- templates/Admin/Dashboard/index.php
+++ templates/Admin/Dashboard/index.php
@@ -5,64 +5,7 @@
  */
 ?>
 
-
-<!-- <section class="content">
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12">
-        
-        <div class="card">
-          <div class="card-header">
-            <h3 class="card-title">Subscriptions</h3>
-            <div class="card-tools">
-              <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
-                <i class="fas fa-minus"></i>
-              </button>
-              <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
-                <i class="fas fa-times"></i>
-              </button>
-            </div>
-          </div>
-          <div class="card-body">
-            Start Date: 01/01/2023
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section> -->
-<style>
-  .box {
-    width: 30%;
-    box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-    position: relative;
-  }
-
-  .btn-info {
-    background-color: #08132F !important;
-    color: #fff !important;
-    border-color: #08132F !important;
-  }
-
-  .btn-info:hover {
-    background-color: #fff !important;
-    color: #08132F !important;
-  }
-
-  .btn-info {
-    position: absolute;
-    left: 9px;
-    top: 0;
-  }
-
-  .pointer {
-    cursor: pointer;
-  }
-
-  .hide {
-    display: none;
-  }
-</style>
+<?= $this->Html->css('admincss') ?>
 <div class="content">
   <div class="row">
     <div class="col-12 landing">
@@ -118,7 +61,67 @@
         <div class="card-header">
           <h5>SETTINGS</h5>
         </div>
-        <div class="card-body"></div>
+        <div class="card-body">
+          <div class="col-md-12">
+            <div class="settings form content">
+              <form method="post" accept-charset="utf-8" action="/bsms/admin/settings/update">
+                <div style="display:none;"><input type="hidden" name="_method" value="PUT"><input type="hidden"
+                    name="_csrfToken" autocomplete="off"
+                    value="QHPE9oRzqNipo31TSZCZCPAg7PGpdxR3cdNlNXQiOTbRpXnc805rp/penuPNILFZN47r0rDStDYLOg67aDmFiEPCQai+cMlFyRKNl5BPI0FJ4FRRQRABV+mWHL/GLQifzjuv2q8Qg2oGt9ziievRBw==">
+                </div>
+                <fieldset>
+                  <div class="row">
+                    <div class="col-4 mb-3">
+                      <div class="input text"><label for="sms-url">Sms Url</label><input type="text" name="sms_url"
+                          class="custom-select rounded-0" div="form-group" id="sms-url"
+                          value="https://www.fast2sms.com/dev/bulkV2"></div>
+                    </div>
+                    <div class="col-4 mb-3">
+                      <div class="input text"><label for="sms-api-key">Sms Api Key</label><input type="text"
+                          name="sms_api_key" class="custom-select rounded-0" div="form-group" id="sms-api-key"
+                          value="TUJOiyzGtxRpCSM5wu4QvFgs2onN19mAecDPZ37Y6XHWkjlE8K3VEDCRNMLb02gX1pYFqn5mo9vIke6J">
+                      </div>
+                    </div>
+                    <div class="col-4 mb-3">
+                      <div class="input text"><label for="sap-url">Sap Url</label><input type="text" name="sap_url"
+                          class="custom-select rounded-0" div="form-group" id="sap-url" value="http://123.108.46.252">
+                      </div>
+                    </div>
+                    <div class="col-4 mb-3">
+
+                      <div class="input text"><label for="sap-segment">Sap Segment</label><input type="text"
+                          name="sap_segment" class="custom-select rounded-0" div="form-group" id="sap-segment"
+                          value="sap/bc/sftmob/VENDER_UPD"></div>
+                    </div>
+                    <div class="col-4 mb-3">
+                      <div class="input text"><label for="sap-client">Sap Client</label><input type="text"
+                          name="sap_client" class="custom-select rounded-0" div="form-group" id="sap-client"
+                          value="300"></div>
+                    </div>
+                    <div class="col-4 mb-3">
+                      <div class="input text"><label for="sap-port">Sap Port</label><input type="text" name="sap_port"
+                          class="custom-select rounded-0" div="form-group" id="sap-port" value="8000"></div>
+                    </div>
+                    <div class="col-4 mb-3">
+                      <div class="input text"><label for="sap-username">Sap Username</label><input type="text"
+                          name="sap_username" class="custom-select rounded-0" div="form-group" id="sap-username"
+                          value="vcsupport1"></div>
+                    </div>
+                    <div class="col-4 mb-3">
+
+                      <div class="input text"><label for="sap-password">Sap Password</label><input type="text"
+                          name="sap_password" class="custom-select rounded-0" div="form-group" id="sap-password"
+                          value="aarti@123"></div>
+                    </div>
+                    <div class="col-md-12">
+                      <button class="btn btn-info" type="submit">Submit</button>
+                    </div>
+                  </div>
+
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="col-8 usermgm hide">
@@ -169,14 +172,4 @@
     </div>
   </div>
 </div>
-<script>
-  $(document).on("click", ".switchcard", function () {
-    $(".landing, .usermgm").hide();
-    $(".setting, .sidecard").show();
-  });
-
-  $(document).on("click", ".usermgm", function () {
-    $(".landing, .setting").hide();
-    $(".usermgm, .sidecard").show();
-  });
-</script>
+<?= $this->Html->script('/js/adminjs.js') ?>
