72ba983151ca13468cb841fb12f52e6dc62293a8
snehal-ft <snehalb@fts-pl.com>
2023-06-08 18:37:44 +0530

    sidebar DEV,PRD

--- webroot/js/adminjs.js
+++ webroot/js/adminjs.js
@@ -3,11 +3,11 @@ $(document).on("click", ".settings", function () {
     if($(this).data('prd') == 1){
         $(".prd_menu_setting, .prd_label").show();
         $(".dev_menu_setting, .dev_label").hide();
-        $(".user-panel").html(`<div class="info"><a class="d-block">PRODUCTION</a></div>`);
+        $(".user-panel").html(`<div class="hello"></div><div class="info"><a class="d-block">PRODUCTION</a></div>`);
     } else {
         $(".dev_menu_setting, .dev_label").show();
         $(".prd_menu_setting, .prd_label").hide();
-        $(".user-panel").html(`<div class="info"><a class="d-block">DEVELOPMENT</a></div>`);
+        $(".user-panel").html(`<div class="world"></div><div class="info"><a class="d-block">DEVELOPMENT</a></div>`);
     }
     $(".setting, .sidecard").show();
 });
