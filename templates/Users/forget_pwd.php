c511281ddc08363afea641f21edb7450ac83aaa9
deepaksingh0207 <deepaksingh0207@gmail.com>
2023-09-11 18:37:23 +0530

    Update forget_pwd.php

--- templates/Users/forget_pwd.php
+++ templates/Users/forget_pwd.php
@@ -118,13 +118,9 @@
                 </div>
                 <div class="forget-buttons">
                 
-<<<<<<< Updated upstream
                 <?php echo $this->Form->button('Submit', array('class' => 'btn btn-custom mt-3', 'style' => 'color:#FFF!important')); ?>
                 <a href="<?= $this->Url->build('/') ?>" class="btn mt-3" style="color: #174071 !important; text-decoration: underline;">Login</a>
-=======
-                <?php echo $this->Form->button('Submit', array('class' => 'btn btn-custom mt-3')); ?>
-                <a href="/bsms" class="btn mt-3">Login</a>
->>>>>>> Stashed changes
+
                 </div>
                 <?= $this->Form->end() ?>
                 <!-- <img src="<?= $this->Url->build('/') ?>img/ft-icon.png" class="ft-icon"> -->
