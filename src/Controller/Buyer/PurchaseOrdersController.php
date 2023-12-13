49953cc3c457ef60b69743e0773f001d3edb250f
jthayil-ft <jonest@fts-pl.com>
2023-12-13 15:13:18 +0530

    Delivery date Filter

--- src/Controller/Buyer/PurchaseOrdersController.php
+++ src/Controller/Buyer/PurchaseOrdersController.php
@@ -105,6 +105,11 @@ class PurchaseOrdersController extends BuyerAppController
                 if(!isset($request['material']) and !isset($request['vendor']) and !isset($request['vendortype']) and !isset($request['segment']) and !isset($request['status']) and !isset($request['po_no'])){ $conditions .= " and po_headers.created_on>='".$search." 00:00:00'"; }
                 else{ $conditions .= " and po_headers.created_on>='".$search." 00:00:00'"; }
             }
+            if(isset($request['delivery_date']) && !empty($request['delivery_date'])) {
+                $search = $request['delivery_date'];
+                if(!isset($request['material']) and !isset($request['vendor']) and !isset($request['vendortype']) and !isset($request['segment']) and !isset($request['status']) and !isset($request['po_no'])){ $conditions .= " and po_item_schedules.delivery_date >='".$search." 00:00:00'"; }
+                else{ $conditions .= " and po_item_schedules.delivery_date >='".$search." 00:00:00'"; }
+            }
             if(isset($request['status'])) {
                 $search = '';
                 foreach ($request['status'] as $mat) { $search .= "'" . $mat . "',"; }
