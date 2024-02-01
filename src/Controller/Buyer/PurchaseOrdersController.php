d291cbf54759600a85077cccb4091b6b453edc44
jthayil-ft <jonest@fts-pl.com>
2024-02-01 11:58:33 +0530

    01.02.24
    Material Search By Code

--- src/Controller/Buyer/PurchaseOrdersController.php
+++ src/Controller/Buyer/PurchaseOrdersController.php
@@ -182,7 +182,7 @@ class PurchaseOrdersController extends BuyerAppController
         $this->loadModel("VendorTemps");
         $this->loadModel('VendorTypes');
         $this->loadModel('Materials');
-        $materials = $this->Materials->find('all')->toArray();
+        $materials = $this->Materials->find('all')->select(['code', 'description'])->distinct(['code', 'description'])->toArray();
         $segment = $this->Materials->find('all')->select(['segment'])->distinct(['segment'])->where(['segment IS NOT NULL' ])->toArray();
         $vendor = $this->VendorTemps->find('all')->select(['sap_vendor_code', 'name'])->distinct(['sap_vendor_code'])->where(['sap_vendor_code IS NOT NULL' ])->toArray();
         $vendortype = $this->Materials->find('all')->distinct(['type'])->where(['type IS NOT NULL' ])->toArray();
@@ -213,8 +213,8 @@ class PurchaseOrdersController extends BuyerAppController
                 $search = '';
                 foreach ($request['material'] as $mat) { $search .= "'" . $mat . "',"; }
                 $search = rtrim($search, ',');
-                if(!isset($request['vendor'])){ $conditions .= " and materials.id in (".$search.")"; }
-                else{ $conditions .= " and materials.id in (".$search.")"; }
+                if(!isset($request['vendor'])){ $conditions .= " and materials.code in (".$search.")"; }
+                else{ $conditions .= " and materials.code in (".$search.")"; }
             }
             if(isset($request['vendortype'])) {
                 $search = '';
@@ -367,7 +367,7 @@ class PurchaseOrdersController extends BuyerAppController
         $this->loadModel("VendorTemps");
         $this->loadModel('VendorTypes');
         $this->loadModel('Materials');
-        $materials = $this->Materials->find('all')->toArray();
+        $materials = $this->Materials->find('all')->select(['code', 'description'])->distinct(['code', 'description'])->toArray();
         $segment = $this->Materials->find('all')->select(['segment'])->distinct(['segment'])->where(['segment IS NOT NULL' ])->toArray();
         $vendor = $this->VendorTemps->find('all')->select(['sap_vendor_code', 'name'])->distinct(['sap_vendor_code'])->where(['sap_vendor_code IS NOT NULL' ])->toArray();
         $vendortype = $this->Materials->find('all')->distinct(['type'])->where(['type IS NOT NULL' ])->toArray();
@@ -398,8 +398,8 @@ class PurchaseOrdersController extends BuyerAppController
                 $search = '';
                 foreach ($request['material'] as $mat) { $search .= "'" . $mat . "',"; }
                 $search = rtrim($search, ',');
-                if(!isset($request['vendor'])){ $conditions .= " and materials.id in (".$search.")"; }
-                else{ $conditions .= " and materials.id in (".$search.")"; }
+                if(!isset($request['vendor'])){ $conditions .= " and materials.code in (".$search.")"; }
+                else{ $conditions .= " and materials.code in (".$search.")"; }
             }
             if(isset($request['vendortype'])) {
                 $search = '';
