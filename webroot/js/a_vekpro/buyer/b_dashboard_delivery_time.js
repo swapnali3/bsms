b6adef32e9a680c49d35b61bd7ff0e7f7a7fe412
surajg100 <141729353+surajg100@users.noreply.github.com>
2024-03-26 11:00:06 +0530

    UI Commits

---
+++ webroot/js/a_vekpro/buyer/b_dashboard_delivery_time.js
@@ -0,0 +1,107 @@
+var chart = AmCharts.makeChart("chartdiv2", {
+    "type": "serial",
+	"theme": "none",
+    "legend": {
+        "horizontalGap": 10,
+        "maxColumns": 1,
+        "position": "right",
+		"useGraphSettings": true,
+		"markerSize": 10
+    },
+    "dataProvider": [{
+        "year": 2003,
+        "europe": 2.5,
+        "namerica": 2.5,
+        "asia": 2.1,
+        "lamerica": 0.3,
+        "meast": 0.2,
+        "africa": 0.1
+    }, {
+        "year": 2004,
+        "europe": 2.6,
+        "namerica": 2.7,
+        "asia": 2.2,
+        "lamerica": 0.3,
+        "meast": 0.3,
+        "africa": 0.1
+    }, {
+        "year": 2005,
+        "europe": 2.8,
+        "namerica": 2.9,
+        "asia": 2.4,
+        "lamerica": 0.3,
+        "meast": 0.3,
+        "africa": 0.1
+    }],
+    "valueAxes": [{
+        "stackType": "regular",
+        "axisAlpha": 0.3,
+        "gridAlpha": 0
+    }],
+    "graphs": [{
+        "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
+        "fillAlphas": 0.8,
+        "labelText": "[[value]]",
+        "lineAlpha": 0.3,
+        "title": "Europe",
+        "type": "column",
+		"color": "#000000",
+        "valueField": "europe"
+    }, {
+        "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
+        "fillAlphas": 0.8,
+        "labelText": "[[value]]",
+        "lineAlpha": 0.3,
+        "title": "North America",
+        "type": "column",
+		"color": "#000000",
+        "valueField": "namerica"
+    }, {
+        "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
+        "fillAlphas": 0.8,
+        "labelText": "[[value]]",
+        "lineAlpha": 0.3,
+        "title": "Asia-Pacific",
+        "type": "column",
+		"color": "#000000",
+        "valueField": "asia"
+    }, {
+        "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
+        "fillAlphas": 0.8,
+        "labelText": "[[value]]",
+        "lineAlpha": 0.3,
+        "title": "Latin America",
+        "type": "column",
+		"color": "#000000",
+        "valueField": "lamerica"
+    }, {
+        "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
+        "fillAlphas": 0.8,
+        "labelText": "[[value]]",
+        "lineAlpha": 0.3,
+        "title": "Middle-East",
+        "type": "column",
+		"color": "#000000",
+        "valueField": "meast"
+    }, {
+        "balloonText": "<b>[[title]]</b><br><span style='font-size:14px'>[[category]]: <b>[[value]]</b></span>",
+        "fillAlphas": 0.8,
+        "labelText": "[[value]]",
+        "lineAlpha": 0.3,
+        "title": "Africa",
+        "type": "column",
+		"color": "#000000",
+        "valueField": "africa"
+    }],
+    "categoryField": "year",
+    "categoryAxis": {
+        "gridPosition": "start",
+        "axisAlpha": 0,
+        "gridAlpha": 0,
+        "position": "left"
+    },
+    "export": {
+    	"enabled": true
+     }
+
+});
