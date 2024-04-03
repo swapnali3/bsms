b00a3fba16341aed7510a24746cee58138f2857c
surajg100 <141729353+surajg100@users.noreply.github.com>
2024-04-03 16:07:28 +0530

    UI Commit

--- webroot/js/a_vekpro/buyer/b_dashboard_purchase_volume.js
+++ webroot/js/a_vekpro/buyer/b_dashboard_purchase_volume.js
@@ -5,6 +5,13 @@ var series1 = chart1.series.push(am5percent.PieSeries.new(root1, {
   valueField: "value",
   categoryField: "category"
 }));
+series1.get("colors").set("colors", [
+  am5.color(0xF4B678),
+  am5.color(0xEF9234),
+  am5.color(0xFFBF00),
+  am5.color(0xFBCEB1),
+  am5.color(0xFF7F50)
+]);
 series1.data.setAll(purchase_volume_segment_wise);
 function refresh_purchasevolume(data) { series1.data.setAll(data); }
 series1.appear(1000, 100);
